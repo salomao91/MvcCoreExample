c5d0ef7fcfe57b855ae0530e8a7d9b50b2933eaa
Salomao Ferreira <salomao1991@gmail.com>
2024-09-20 11:29:39 -0400

    add StudentController

---
+++ MvcCoreExample/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreExample.Models;
+
+namespace MvcCoreExample.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
