16c8cc41871a6e599c5fa1f5bcb909d927300a76
Salomao Ferreira <salomao1991@gmail.com>
2024-09-20 12:28:57 -0400

    add GetAll method in the StudentController

--- MvcCoreExample/Controllers/StudentController.cs
+++ MvcCoreExample/Controllers/StudentController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using MvcCoreExample.Models;
+using System.Data.SqlClient;
 
 namespace MvcCoreExample.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly string _connectionString;
+
+        public StudentController(IConfiguration connectionString)
+        {
+            _connectionString = connectionString.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string query = "SELECT * FROM Student";
+                var students = await sqlConnection.QueryAsync<StudentModel>(query);
+                return Ok(students);
+            }
         }
 
     }
