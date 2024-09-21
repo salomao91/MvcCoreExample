1b2f9d472bbf2470cedcfc0d9d400a3939f7c266
Salomao Ferreira <salomao1991@gmail.com>
2024-09-21 19:44:00 -0400

    add GetAll method and rendered professors return in a data table view

---
+++ MvcCoreExample/Controllers/ProfessorController.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreExample.Models;
+using System.Data.SqlClient;
+
+namespace MvcCoreExample.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        private readonly string _connectionString;
+
+        public ProfessorController(IConfiguration connectionString)
+        {
+            _connectionString = connectionString.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string query = "SELECT * FROM Professor";
+                var professors = await sqlConnection.QueryAsync<ProfessorModel>(query);
+                return View(professors);
+            }
+        }
+    }
+}
