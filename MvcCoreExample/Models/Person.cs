e5fe076392a3f04438c5de6f57baeff8fb449d9e
Salomao Ferreira <salomao1991@gmail.com>
2024-09-21 17:58:33 -0400

    fix: Person model properties

--- MvcCoreExample/Models/Person.cs
+++ MvcCoreExample/Models/Person.cs
@@ -4,8 +4,9 @@ namespace MvcCoreExample
 {
     public class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime Birthdate { get; set; }
         public string SocialNumber { get; set; }
         public AddressModel Address { get; set; }
     }
